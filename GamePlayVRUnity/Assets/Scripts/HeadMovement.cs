3d64bbf88da22bdd87d5ac6b01f35925f1367466
Dylaann <c00204772@itcarlow.ie>
2017-04-27 15:41:08 +0100

    unity game

---
+++ GamePlayVRUnity/Assets/Scripts/HeadMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadMovement : MonoBehaviour
+{
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    void Update()
+    {
+        var moveX = Input.acceleration.x;
+
+        var x = Input.gyro.rotationRateUnbiased.x;
+        var y = Input.gyro.rotationRateUnbiased.y;
+        var z = Input.gyro.rotationRateUnbiased.z;
+
+        transform.Rotate(-x, -y, z);
+        
+
+    }
+}
---
+++ GamePlayVRUnity/Assets/Scripts/movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class movement : MonoBehaviour {
+
+    // gravity constant
+    public float g = 9.8f;
+
+    void FixedUpdate()
+    {
+        // normalize axis
+        var gravity = new Vector3(
+           Input.acceleration.x,
+            0,
+            0
+        ) * g;
+
+		if (GetComponent<Rigidbody> ().position.x < 8 && GetComponent<Rigidbody> ().position.x > -8) 
+		{
+			GetComponent<Rigidbody> ().AddForce (gravity, ForceMode.Force);
+		} 
+		else if (GetComponent<Rigidbody> ().position.x > 9) 
+		{
+			GetComponent<Rigidbody> ().AddForce (-0.5F, 0, 0, ForceMode.Impulse);
+		}
+		else if (GetComponent<Rigidbody> ().position.x < -9) 
+		{
+			GetComponent<Rigidbody> ().AddForce (0.5F, 0, 0, ForceMode.Impulse);
+		}
+    }
+
+}
